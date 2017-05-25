0f191a57b8a0f60b5baf6ffc9aeee6f7174d6c77
Yoimer <ydrf19@gmail.com>
2017-05-25 10:34:37 -0400

    Added PINOUT Connection section and modified identation on sim800l-simpleserialcommunication folder

--- sim800l-simpleserialcommunication/sim800l-simpleserialcommunication.ino
+++ sim800l-simpleserialcommunication/sim800l-simpleserialcommunication.ino
@@ -10,8 +10,47 @@
  
 //Create software serial object to communicate with SIM800
 SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
- 
-void setup() {
+
+
+/*
+
+PINOUT Connection:
+
+///////////////////////////////////////////////////////////////////////////////
+
+External 12VDC/2A Power Supply                                    MP1584 (Turn knot until a volmeter shows 5VDC)
+
+Positive--------------------------------------------------------->Positive
+
+Negative--------------------------------------------------------->Negative
+
+///////////////////////////////////////////////////////////////////////////////
+
+MP1584                                                            SIM800L-EVB
+
+Positive--------------------------------------------------------->5V/4V
+
+Negative--------------------------------------------------------->GNB
+
+///////////////////////////////////////////////////////////////////////////////
+
+Arduino UNO                                                        SIM800L-EVB
+
+Digital 7--------------------------------------------------------->SIM_RXD
+
+Digital 8--------------------------------------------------------->SIM_TX
+
+RESET------------------------------------------------------------->RST
+
+GND (POWER SECTION)----------------------------------------------->GND
+
+///////////////////////////////////////////////////////////////////////////////
+
+*/
+
+
+void setup()
+ {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
   while(!Serial);
@@ -23,13 +62,16 @@ void setup() {
   Serial.println("Setup Complete!");
 }
  
-void loop() {
+void loop()
+{
   //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-  if(serialSIM800.available()){
+  if(serialSIM800.available())
+  {
     Serial.write(serialSIM800.read());
   }
   //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-  if(Serial.available()){    
+  if(Serial.available())
+  {    
     serialSIM800.write(Serial.read());
   }
 }
